ff6a97d605e35c82f63b2cfdd863e9107317b0e9
Sergey Pustovalov <pustserg@yandex.ru>
2016-05-04 12:30:10 +0300

    Отправка текста на проверку

--- lib/text_ru_api/request.rb
+++ lib/text_ru_api/request.rb
@@ -1,7 +1,13 @@
 module TextRuApi
   class Request
+    URL = 'http://api.text.ru/post'.freeze
     def initialize(access_token = nil)
       @access_token = access_token || ENV['text_ru_api_token']
+    end
+
+    def result(options = {})
+      data = { userkey: @access_token }.merge(options)
+      HTTParty.post(URL, body: data)
     end
   end
 end
